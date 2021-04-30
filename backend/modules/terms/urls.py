cded39742c325db10eba2f9d32222a57b7aa9def
Ben Goldstein <ben@bens-mbp-3.home>
2021-04-29 17:27:51 -0700

    commenting out views calls from urls.py to fix migration issue

--- backend/modules/terms/urls.py
+++ backend/modules/terms/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .viewsets import TermAndConditionViewSet
+# from .viewsets import TermAndConditionViewSet
 
 
 router = DefaultRouter()
-router.register("termsandconditions", TermAndConditionViewSet)
+# router.register("termsandconditions", TermAndConditionViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
